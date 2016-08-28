88cc3b85b34dd781759e657583c868027d82d0b0
Rita Lu <rita@Ritas-MacBook-Air.local>
2016-08-27 17:52:58 -0700

    Redid migrations, added Resources seed

--- hello/models.py
+++ hello/models.py
@@ -1,24 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
-
-
 class Stories(models.Model): 
     html_text = models.TextField(null=True, blank=True)
     timestamp = models.DateTimeField()  # Client timestamp
-    longitude = models.DecimalField(max_digits=15, decimal_places=15)
-    latitude = models.DecimalField(max_digits=15, decimal_places=15)
+    longitude = models.DecimalField(max_digits=20, decimal_places=10)
+    latitude = models.DecimalField(max_digits=20, decimal_places=10)
 
 class Resources(models.Model):
-	latitude = models.DecimalField(max_digits=15, decimal_places=15)
-	longitude = models.DecimalField(max_digits=15, decimal_places=15)
+	latitude = models.DecimalField(max_digits=20, decimal_places=10)
+	longitude = models.DecimalField(max_digits=20, decimal_places=10)
 	name = models.TextField()
 	address = models.TextField()
 	city = models.TextField()
 	state = models.TextField()
 	country = models.TextField()
+	zip_code = models.TextField()
 	description = models.TextField(null=True, blank=True)
 	phone_number = models.TextField()
 	url = models.TextField()
