f44bccf9c5bc1951e871b5a35a4bcae1396520ce
Rita Lu <rita.lu410@gmail.com>
2016-08-28 01:33:38 -0700

    Added API to create resource

--- hello/models.py
+++ hello/models.py
@@ -31,7 +31,7 @@ class Stories(models.Model):
 class Resources(models.Model):
 	latitude = models.DecimalField(max_digits=20, decimal_places=10)
 	longitude = models.DecimalField(max_digits=20, decimal_places=10)
-	place_id = models.TextField()
+	place_id = models.TextField(null=True, blank=True)
 	name = models.TextField()
 	address = models.TextField(null=True, blank=True)
 	city = models.TextField(null=True, blank=True)
