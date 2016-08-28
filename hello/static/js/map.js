981e2fc636840103654121d48412523768551640
Anastasia Zorko <azorko13@gmail.com>
2016-08-28 09:23:06 -0700

    start exapandable pin info window

--- hello/static/js/map.js
+++ hello/static/js/map.js
@@ -18,24 +18,24 @@ class StoryMap {
       scrollwheel: false,
       zoom: 12
     });
-    var stories;
-    var resources;
-    var storyPin = new google.maps.MarkerImage("/static/pins_stories.png");
-    var resourcePin = new google.maps.MarkerImage("/static/pins_resource.png");
-    var stories_query = jQuery.ajax("/api/stories?lat=" + lat + "&lon=" + lng);
-    stories_query.done(
-      function(stories){
-        this.stories = this.addMarkers(JSON.parse(stories),
-        storyPin, true
-    );
-      }.bind(this));
-    var resources_queries = jQuery.ajax("/api/resources?lat=" + lat + "&lon=" + lng);
-    resources_queries.done(
-      function(resources){
-        this.resources = this.addMarkers(JSON.parse(resources),
-        resourcePin, false
-    );
-      }.bind(this));
+    var storyPin = new google.maps.MarkerImage("/static/pins_resource.png");
+    var resourcePin = new google.maps.MarkerImage("/static/pins_stories.png");
+    this.stories = this.addMarkers([
+      {lat: 37.422327, lng: -122.084401, assailant: 'domestic', type_of_abuse: 'rape', gender: 'female', location: 'college campus', reported: 'yes', pk: 1}], storyPin, true);
+    // var stories_query = jQuery.ajax("/api/stories?lat=" + lat + "&lon=" + lng);
+    // stories_query.done(
+    //   function(stories){
+    //     this.stories = this.addMarkers(JSON.parse(stories),
+    //     storyPin, true
+    // );
+    //   }.bind(this));
+    // var resources_queries = jQuery.ajax("/api/resources?lat=" + lat + "&lon=" + lng);
+    // resources_queries.done(
+    //   function(resources){
+    //     this.resources = this.addMarkers(JSON.parse(resources),
+    //     resourcePin, false
+    // );
+    //   }.bind(this));
 
     this.setEventHandlers();
   }
@@ -48,6 +48,16 @@ class StoryMap {
     });
   }
 
+  filterByField(objects, fieldName, fieldValue) {
+    var matches = [];
+    for (let object of objects) {
+      if (object[fieldName] === fieldValue) {
+        matches.push(object);
+      }
+    }
+    return matches;
+  }
+
   togglePins(type, evt) {
     var $target = $(evt.currentTarget);
     $target.toggleClass('selected');
@@ -58,30 +68,32 @@ class StoryMap {
   // Adds a marker to the map.
   addMarkers(locations, pinImg, showInfoWindow) {
     var markers = [];
-    for (let location_object of locations) {
-      var location = location_object.fields;
+    for (let location of locations) { //location_object
+      // var location = location_object.fields;
       let marker = new google.maps.Marker({
         position: {lat: parseFloat(location.lat), lng: parseFloat(location.lng)},
         map: null,
         icon: pinImg,
-        markerId: location.id,
         animation: google.maps.Animation.DROP,
       });
+      marker = Object.assign(marker, location);
       if (showInfoWindow) {
         var fbLink = "";
         if (location.post_id) {
           var post_tags = location.post_id.split("_");
           fbLink = 'https://www.facebook.com/permalink.php?story_fbid=' + post_tags[1] + '&id=' + post_tags[0];
         }
-
-
+        var reported = location.report === 'yes' ? 'reported' : 'not reported';
         let infowindow = new google.maps.InfoWindow({
-          content: '<div class="story-bubble" data-id="'+marker.markerId+'">'+
+          content: '<div class="story-bubble">'+
             '<div class="map-filter">'+location.assailant+'</div>'+
             '<div class="map-filter">'+location.type_of_abuse+'</div>'+
             '<div class="map-filter">'+location.gender+'</div>'+
             '<a href="'+fbLink+'" class="fb-link"><img src="/static/facebook_logo.png"></a>'+
-            '<button class="btn btn-default read-story-btn">read story</button>'
+            '<div class="map-filter">'+location.location+'</div>'+
+            '<div class="map-filter">'+reported+'</div>'+
+            '<div class="story-text">'+location.story+'</div>'+
+            '<button class="btn btn-default read-story-btn">read story</button></div>'
         });
         marker.addListener('click', function() {
           if (this.openWindow) {
